2874bcd984f25b047deefef4b4f35cf3cb2f6bee
panguicheng <pangc@segimail.com>
2020-01-04 11:18:51 +0800

    update

---
+++ src/com/abc/thread/Fight/FightQuery.java
@@ -0,0 +1,12 @@
+package com.abc.thread.Fight;
+
+import java.util.List;
+
+/**
+ * @author abcpan
+ * @version 1.0
+ * @date 2020/1/4 10:10
+ */
+public interface FightQuery {
+  List<String>  get();
+}
